34b0e2ce43233a2a43d1a56c1153711f81b608aa
bjxiaowen <bjxiaowen@126.com>
2016-05-31 01:01:27 +0800

    修改了数据类型

--- src/com/PSMS/util/BiPowerStationTools.java
+++ src/com/PSMS/util/BiPowerStationTools.java
@@ -14,7 +14,7 @@ public class BiPowerStationTools {
 	 */
 	public static  List<PowerStationBase> getListSize24(List<PowerStationBase> list){
 		List<PowerStationBase> reList=new ArrayList<PowerStationBase>();
-		for(int i=0;i<24;i++){
+		for(int i=0;i<23;i++){
 			boolean flag=true;
 			for(PowerStationBase power: list){
 				int hour=power.getGroupHour();
